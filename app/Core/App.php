4a847b256081d7bb69b4236f1459cf3baa23f266
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-07-06 20:02:50 +0300

    Refactor: Registered the Authenticate middleware
    Registered the Authenticate middleware to verify that the user is authenticated.

--- app/Core/App.php
+++ app/Core/App.php
@@ -4,12 +4,26 @@ namespace App\Core;
 use App\Core\Utility\UrlHelper;
 
 class App {
-	protected $controller = "App\Controllers\Home";
+	protected $controller = "Home";
 	protected $method = "index";
 	protected $params = [];
+	protected $middlewares = [
+		'Accounts/setModerator' => ['ensureLoggedin@Authenticate'],
+		'Accounts/removeModerator' => ['ensureLoggedin@Authenticate'],
+		'Accounts/updatePassword' => ['ensureLoggedin@Authenticate'],
+		'Comments/createComment' => ['ensureLoggedin@Authenticate'],
+		'Comments/updateComment' => ['ensureLoggedin@Authenticate'],
+		'Comments/deleteComment' => ['ensureLoggedin@Authenticate'],
+		'Login/index' => ['ensureNotLoggedin@Authenticate'],
+		'Posts/createPost' => ['ensureLoggedin@Authenticate'],
+		'Posts/updatePost' => ['ensureLoggedin@Authenticate'],
+		'Posts/deletePost' => ['ensureLoggedin@Authenticate'],
+		'Signup/index' => ['ensureNotLoggedin@Authenticate']
+	];
 
 	public function __construct() {
 		$this->initializeDestination();
+		$this->checkMiddleware();
 		$this->runController();
 	}
 
@@ -17,18 +31,33 @@ class App {
 		$urlHelper = new UrlHelper();
 		$destinationData = $urlHelper->fetchUrlArguments();
 
-		$this->setController('App\\Controllers\\' . $destinationData['controller']);
+		$this->setController($destinationData['controller']);
 		$this->setControllerMethod($destinationData['method']);
 		$this->params = $destinationData['params'];
 	}
 
 	private function runController() {
-		$controllerObject = new $this->controller;
-		call_user_func_array([$controllerObject, $this->method], $this->params);
+		$controllerClassName = 'App\Controllers\\' . $this->controller;
+		
+		call_user_func_array([new $controllerClassName, $this->method], $this->params);
+	}
+
+	public function checkMiddleware() {
+		$destination = $this->controller . '/' . $this->method;
+
+		if (array_key_exists($destination, $this->middlewares) === true) {
+			foreach ($this->middlewares[$destination] as $middleware) {
+				$middlewareDestination = explode('@', $this->middlewares[$destination][0]);
+				$middlewareClassName = 'App\Core\Middleware\\' . $middlewareDestination[1];
+				$middlewareMethod = $middlewareDestination[0];
+
+				call_user_func([$middlewareClassName, $middlewareMethod]);
+			}
+		}
 	}
 
 	private function setController($controller) {
-		if (class_exists($controller) === true) {
+		if (class_exists('App\Controllers\\' . $controller) === true) {
 			$this->controller = $controller;
 			return true;
 		}
@@ -38,9 +67,9 @@ class App {
 	}
 
 	private function setControllerMethod($method) {
-		$controllerObject = new $this->controller;
+		$controllerClassName = 'App\Controllers\\' . $this->controller;
 
-		if (method_exists($controllerObject, $method)) {
+		if (method_exists(new $controllerClassName, $method)) {
 			$this->method = $method;
 			return true;
 		}
