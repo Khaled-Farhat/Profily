2cf26bf13b62590858282d3eb06392df17b93aa2
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-07-06 07:15:13 +0300

    Style: Fixing the indention

--- app/Views/Posts/viewPost.php
+++ app/Views/Posts/viewPost.php
@@ -1,59 +1,59 @@
 <div class="container-fluid bg-light py-4">
-	<div class="container">
-		<div class="shadow-sm card col-12 my-3">
-			<div class="card-body">
-			 	<h5 class="card-title"><?= htmlentities($data['post']->title) ?></h5>
+    <div class="container">
+        <div class="shadow-sm card col-12 my-3">
+            <div class="card-body">
+                <h5 class="card-title"><?= htmlentities($data['post']->title) ?></h5>
 
-				<span class="card-subtitle text-muted">Author: </span>
-				<a href="<?= URL . '/Profile/viewProfile/' . $data['post']->user->username ?>" class="card-subtitle mb-2 text-muted"><?= $data['post']->user->username ?></a>
-				<span>|  </span>
+                <span class="card-subtitle text-muted">Author: </span>
+                <a href="<?= URL . '/Profile/viewProfile/' . $data['post']->user->username ?>" class="card-subtitle mb-2 text-muted"><?= $data['post']->user->username ?></a>
+                <span>|  </span>
 
-			    <span class="card-subtitle mb-2 text-muted">Last update: <?= date('Y-M-d h:i', strtotime($data['post']->updated_at)) ?></span>
+                <span class="card-subtitle mb-2 text-muted">Last update: <?= date('Y-M-d h:i', strtotime($data['post']->updated_at)) ?></span>
 
-			    <p class="card-text my-4 px-3"><?= nl2br(htmlentities($data['post']->content)) ?></p>
+                <p class="card-text my-4 px-3"><?= nl2br(htmlentities($data['post']->content)) ?></p>
 
-			    <a href="<?= URL . '/Comments/createComment/' . $data['post']->id  ?>" class="btn btn-primary bg-gradient">Create a comment</a>
+                <a href="<?= URL . '/Comments/createComment/' . $data['post']->id  ?>" class="btn btn-primary bg-gradient">Create a comment</a>
 
-			    <?php
-			    if (isset($data['postAllowedActions']['updatePost']) === true) {
-			    	echo '<a href="' . URL . '/Posts/updatePost/'. $data['post']->id .'" class="btn btn-outline-dark">Edit post</a> ';
-			    }
+                <?php
+                if (isset($data['postAllowedActions']['updatePost']) === true) {
+                    echo '<a href="' . URL . '/Posts/updatePost/'. $data['post']->id .'" class="btn btn-outline-dark">Edit post</a> ';
+                }
 
-			    if (isset($data['postAllowedActions']['deletePost']) === true) {
-			    	echo '<a href="' . URL . '/Posts/deletePost/'. $data['post']->id .'" class="btn btn-outline-dark">Delete post</a>';
-			    }
+                if (isset($data['postAllowedActions']['deletePost']) === true) {
+                    echo '<a href="' . URL . '/Posts/deletePost/'. $data['post']->id .'" class="btn btn-outline-dark">Delete post</a>';
+                }
 
-			    if (count($data['post']->comments) > 0) {
-			    	echo '<h5 class="card-title my-4">Comments:</h5>';
-				}
+                if (count($data['post']->comments) > 0) {
+                    echo '<h5 class="card-title my-4">Comments:</h5>';
+                }
 
-			    foreach($data['post']->comments as $comment) {
-			    ?>
-					<div class="mx-3 mb-3 card">
-						<div class="card-body">
-							<span class="card-subtitle text-muted">Comment by: </span>
-								<a href="<?= URL . '/Profile/viewProfile/' . $comment->user->username ?>" class="card-subtitle mb-2 text-muted"><?= $comment->user->username ?></a>
-							    <span>|  </span>
+                foreach($data['post']->comments as $comment) {
+                ?>
+                    <div class="mx-3 mb-3 card">
+                        <div class="card-body">
+                            <span class="card-subtitle text-muted">Comment by: </span>
+                                <a href="<?= URL . '/Profile/viewProfile/' . $comment->user->username ?>" class="card-subtitle mb-2 text-muted"><?= $comment->user->username ?></a>
+                                <span>|  </span>
 
-							    <span class="card-subtitle mb-2 text-muted">Last update: <?= date('Y-M-d h:i', strtotime($comment->updated_at)) ?></span>
+                                <span class="card-subtitle mb-2 text-muted">Last update: <?= date('Y-M-d h:i', strtotime($comment->updated_at)) ?></span>
 
-							    <p class="card-text mt-3"><?= htmlentities($comment->content) ?></p>
+                                <p class="card-text mt-3"><?= htmlentities($comment->content) ?></p>
 
-							    <?php
-							    	if (isset($data[$comment->id]['commentAllowedActions']['updateComment']) === true) {
-									   	echo '<a href="' . URL . '/Comments/updateComment/'. $comment->id .'" class="btn btn-outline-dark">Edit comment</a> ';
-									    }
+                                <?php
+                                    if (isset($data[$comment->id]['commentAllowedActions']['updateComment']) === true) {
+                                        echo '<a href="' . URL . '/Comments/updateComment/'. $comment->id .'" class="btn btn-outline-dark">Edit comment</a> ';
+                                        }
 
-									if (isset($data[$comment->id]['commentAllowedActions']['deleteComment']) === true) {
-								    	echo '<a href="' . URL . '/Comments/deleteComment/'. $comment->id .'" class="btn btn-outline-dark">Delete comment</a>';
-								    }
-							    ?>
-						</div>
-					</div>
-				<?php
-			    }
-			    ?>
-			</div>
-		</div>
-	</div>
+                                    if (isset($data[$comment->id]['commentAllowedActions']['deleteComment']) === true) {
+                                        echo '<a href="' . URL . '/Comments/deleteComment/'. $comment->id .'" class="btn btn-outline-dark">Delete comment</a>';
+                                    }
+                                ?>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
